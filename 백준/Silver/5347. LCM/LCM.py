fb9b65fc18dafa0cb4aceb2ee01bfed9a7024ed2
j.myeong_ <jmyeong012@gmail.com>
2022-07-10 16:41:17 +0900

    [Silver IV] Title: LCM, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/5347. LCM/LCM.py
@@ -0,0 +1,14 @@
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+test = int(input())
+
+for _ in range(test):
+    tmp = input().split(' ')
+    a, b = int(tmp[0]), int(tmp[1])
+    gcd_ab = gcd(a, b)
+    print(a * b // gcd_ab)
