4fb85318958dfaf027965ec62261e634e5332422
j.myeong_ <jmyeong012@gmail.com>
2023-10-04 16:36:27 +0900

    [level 1] Title: 예산, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    let answer = 0;
+    d.sort(function(a, b){return a-b});
+    for (let i = 0; i < d.length; i++) {
+        if (budget - d[i] >= 0) {
+            budget -= d[i];
+            answer++;
+        }
+        else break;
+    }
+    return answer;
+}
