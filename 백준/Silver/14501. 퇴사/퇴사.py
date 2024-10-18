93a580cd049d7bb4c1885ea5e2d1d194479c46d4
jmyeong <jmyeong012@gmail.com>
2024-10-18 11:56:05 +0000

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,22 +1,20 @@
 import sys
 
-input = sys.stdin.readline
+sys.setrecursionlimit(999999)
 
-def interview(idx, value):
-    global answer
+def interview(idx):
+    if idx > n:
+        return -99999999999
+    if idx == n:
+        return 0
+    if dp[idx] != -1:
+        return dp[idx]
 
-    if idx >= n:
-        if idx > n: return;
-        answer = max(answer, value)
-        return;
-    # 인터뷰 하는 경우
-    interview(idx + data[idx][0], value + data[idx][1])
-    # 인터뷰 안하는 경우
-    interview(idx + 1, value)
+    dp[idx] = max(interview(idx + table[idx][0]) + table[idx][1], interview(idx + 1))
+    return dp[idx]
 
 n = int(input())
-data = [list(map(int, input().strip().split())) for _ in range(n)]
-
-answer = 0
-interview(0, 0)
-print(answer)
+table = [list(map(int, input().split())) for _ in range(n)]
+dp = [-1] * (n + 1)
+interview(0)
+print(max(dp))
